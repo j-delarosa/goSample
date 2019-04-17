5bc0e6dd8945f66936a9c4bccc7331620cfe7a9f
Jonathan de la Rosa <jonadlr@hotmail.com>
2019-04-17 14:31:50 -0700

    adding test play files

---
+++ test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Animal struct {
+	NumberOfLegs int
+	Name         string
+	func (a Animal) GetLegCount() int {
+		return a.NumberOfLegs
+	}
+}
+
+func (doggy Dog) bark() {
+	fmt.Printf("%s went woof woof!!\n", doggy.Name)
+}
+
+type Dog struct {
+	Animal
+}
+
+func main() {
+	dog := Dog{}
+	dog.Name = "timmy"
+	dog.NumberOfLegs = 4
+	fmt.Println(dog.Name)
+	dog.bark()
+	fmt.Println(dog.GetLegCount())
+}
